c0b3447b4698dbb6db44a67fcb818921f4b721b0
Homeomorphistic <chmiela.bartosz@gmail.com>
2026-03-02 16:01:47 +0100

    feat: update examples dataset

--- data-raw/examples.R
+++ data-raw/examples.R
@@ -1,7 +1,10 @@
 examples <- tidyquant::tq_get(
-  c("CDR.WA", "TSLA", "BTC-USD", "GC=F", "AGGU.L", "VWRA.L"),
-  from = Sys.Date() - 365 * 2,
-  to   = Sys.Date()
+  c("CDR.WA", "ALE.WA", "PKO.WA",
+    "NVDA", "AMZN", "TSLA", "^SP500TR",
+    "BTC-USD", "GC=F",
+    "AGGU.L", "VWRA.L"),
+  from = as.Date("2020-01-01"),
+  to   = as.Date("2026-03-01")
 )
 
 usethis::use_data(examples, overwrite = TRUE)
